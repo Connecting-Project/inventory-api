b173a37a799f9d97ebe5dd5422b6e5682bd3a7e7
soohyunKim <kimsoo5133@gmail.com>
2021-04-08 19:50:20 +0900

    user rent list add

--- src/main/java/com/hawaiianpizza/inventory/dao/DeviceDao.java
+++ src/main/java/com/hawaiianpizza/inventory/dao/DeviceDao.java
@@ -2,6 +2,7 @@ package com.hawaiianpizza.inventory.dao;
 
 import com.hawaiianpizza.inventory.model.Device;
 import com.hawaiianpizza.inventory.model.Products;
+import com.hawaiianpizza.inventory.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +14,6 @@ import java.util.Optional;
 public interface DeviceDao extends JpaRepository<Device, String> {
 
      Optional<Device> findById(int id);
+
+    List<Device> findByUser(User user);
 }
